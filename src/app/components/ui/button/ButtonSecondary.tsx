032c5799b6c13327370c6a7fbefc1a6ca3a74a47
Min Khant <113674028+Rayy-007@users.noreply.github.com>
2024-01-03 08:11:39 +0000

    Fixed Slug Type Error in product page

--- src/app/components/ui/button/ButtonSecondary.tsx
+++ src/app/components/ui/button/ButtonSecondary.tsx
@@ -1,11 +1,11 @@
-import LabelProp from "@/types/global"
+import { LabelProp } from "@/types/global"
 
-export default function ButtonSecondary( {label} : LabelProp){
-        return (
-            <button
-            className= "hover:bg-dark-900 border border-dark-900 text-dark-850 font-bold hover:text-light-100 px-6 py-3"
-            >
-                {label}
-            </button>
-        )
+export default function ButtonSecondary({ label }: LabelProp) {
+    return (
+        <button
+            className="hover:bg-dark-900 border border-dark-900 text-dark-850 font-bold hover:text-light-100 px-6 py-3"
+        >
+            {label}
+        </button>
+    )
 }
--- src/app/components/ui/navigation/Footer.tsx
+++ src/app/components/ui/navigation/Footer.tsx
@@ -20,9 +20,9 @@ export default function Footer() {
           <div className="mt-8 flex flex-col gap-8">
             <p className="w-1/2 opacity-50">
               Audiophile is an all in one stop to fulfill your audio needs.
-              We're a small team of music lovers and sound specialists who are
+              We&apos; a small team of music lovers and sound specialists who are
               devoted to helping you get the most out of personal audio. Come
-              and visit our demo facility - we’re open 7 days a week.
+              and visit our demo facility - we&apos;re open 7 days a week.
             </p>
             <p className="opacity-50">Copyright 2021. All Rights Reserved</p>
           </div>
